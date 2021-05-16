70a71321d8ae619316d94a22f01d9f43233a2f31
Chiro <sachinsinghroxx@gmail.com>
2021-05-16 23:53:50 +0530

    added multi render of memes

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
-  let url = null;
+  let dataURL: Promise<any> | null = null;
   const fetchImage = (async () => {
     const response = await fetch("https://meme-api.herokuapp.com/gimme");
-    return await response.json();
-  })();
 
-  
-  const refreshImage = async () => {
-    const response = await fetch("https://meme-api.herokuapp.com/gimme");
-  };
+    return await response.json();
+  });
+
+  $: memeFetch = fetchImage; 
 </script>
 
-{#await fetchImage}
+{#await memeFetch()}
+  <p>Getting you top quality Meme!</p>
+{:then data}
+  <img src={data.url} alt="Meme" />
+  <p>here it is !</p>
+{:catch}
+  <p>Something went wrong!</p>
+{/await}
+{#await memeFetch()}
   <p>Getting you top quality Meme!</p>
 {:then data}
   <img src={data.url} alt="Meme" />
@@ -20,8 +26,12 @@
   <p>Something went wrong!</p>
 {/await}
 
+
 <!-- <button class:active on:click={() => (active = !active)}>foo</button> -->
 
 <!-- svelte-ignore missing-declaration -->
 <style>
+  p {
+    left:"50%"
+  }
 </style>
