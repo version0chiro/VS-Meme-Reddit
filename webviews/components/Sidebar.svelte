045d93126e78c79c77811a2495cb15b77a7cbd25
Chiro <sachinsinghroxx@gmail.com>
2021-05-17 02:16:00 +0530

    pre css

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -17,14 +17,6 @@
 {:catch}
   <p>Something went wrong!</p>
 {/await}
-{#await memeFetch()}
-  <p>Getting you top quality Meme!</p>
-{:then data}
-  <img src={data.url} alt="Meme" />
-  <p>here it is !</p>
-{:catch}
-  <p>Something went wrong!</p>
-{/await}
 
 
 <!-- <button class:active on:click={() => (active = !active)}>foo</button> -->
