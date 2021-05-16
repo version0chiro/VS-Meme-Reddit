b192b2c50a68331a9c901644b3fae11bf7de3064
Chiro <sachinsinghroxx@gmail.com>
2021-05-16 23:25:40 +0530

    able to show memes on side bar

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,55 +1,19 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-  
-    let todos: Array<{ text: string; completed: boolean }> = [];
-    let text = "";
-  
-    onMount(() => {
-      window.addEventListener("message", (event) => {
-        const message = event.data; // The json data that the extension sent
-        switch (message.type) {
-          case "new-todo":
-            todos = [{text:message.value,completed:false},...todos,];
-            break;
-        }
-      });
-    });
-  </script>
-  
-  <form
-    on:submit|preventDefault={(e) => {
-      todos = [{ text, completed: false }, ...todos];
-      text = "";
-    }}
-  >
-    <input bind:value={text} />
-  </form>
-  
-  <ul>
-    {#each todos as todo (todo.text)}
-      <li
-        class:complete={todo.completed}
-        on:click={() => {
-          todo.completed = !todo.completed;
-        }}
-      >
-        {todo.text}
-      </li>
-    {/each}
-  </ul>
-  
-  <div>text: {text}</div>
-  
-  <!-- svelte-ignore missing-declaration -->
-  
-  
-  <style>
-    .complete {
-      text-decoration: line-through;
-    }
-    div {
-      text-align: center;
-      color: rgb(255, 255, 255);
-    }
-  </style>
-  
+  const fetchImage = (async () => {
+    const response = await fetch("https://meme-api.herokuapp.com/gimme");
+    return await response.json();
+  })();
+</script>
+
+{#await fetchImage}
+  <p>Getting you top quality Meme!</p>
+{:then data}
+  <img src={data.url} alt="Meme" />
+  <p>here it is !</p>
+{:catch}
+  <p>Something went wrong!</p>
+{/await}
+
+<!-- svelte-ignore missing-declaration -->
+<style>
+</style>
